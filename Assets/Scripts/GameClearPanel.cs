c13833f2ace7452dd5f096fc3d7db14be5327e7d
CHA-OMEN\caris <chahyun.dev@gmail.com>
2025-10-19 16:32:33 +0900

    [Feat] 리듬게임 덮어씌움, 게임클리어패널 프리팹추가

--- Assets/Scripts/GameClearPanel.cs
+++ Assets/Scripts/GameClearPanel.cs
@@ -1,3 +1,4 @@
+using DG.Tweening;
 using System.Collections;
 using System.Collections.Generic;
 using TMPro;
@@ -8,15 +9,31 @@ public class GameClearPanel : MonoBehaviour
 {
 
     [Header("UI")]
-    public Image panelGameClear;
+    public Image imageGameClearStatus;
     public Sprite spriteSuccess; // 게임 성공 이미지 
-    public Sprite spriteFail; // 게임 실패 이미지
+    public Sprite spriteFail; // 게임 실패 이미지\
+
+    private DOTweenAnimation DOTWeen;
+
+    public void Start()
+    {
+        gameObject.SetActive(false);
+        DOTWeen = GetComponentInChildren<DOTweenAnimation>();
+    }
 
     // Start is called before the first frame update
     public void ShowGameClearPanel(bool result)
     {
         // true면 성공, fail이면 실패 패널
-        // 성공 실패 기준 알아서
-        panelGameClear.gameObject.SetActive(result);
+        if (result)
+        {
+            imageGameClearStatus.sprite = spriteSuccess;
+        } else
+        {
+            imageGameClearStatus.sprite = spriteFail;
+        }
+
+        gameObject.SetActive(true);
+        DOTWeen.DORestart();
     }
 }
